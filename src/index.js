cc11ef3f0bd8def7ff47fd2a961f2e1b9d0b7910
German <pentagram777@mail.ru>
2020-09-30 12:32:22 +0500

    add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  if (!array || !array.length) {
+    return 0
+  }
+  else {
+    let newArr = array.sort((a, b) => a - b);
+    return newArr[0];
+  }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (!array || !array.length) {
+    return 0
+  }
+  else {
+    let newArr = array.sort((a, b) => a - b);
+    return newArr[newArr.length - 1];
+  }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+};
+
+exports.avg = function avg(array) {
+  if (!array || !array.length) {
+    return 0
+  }
+  else {
+
+    let arr = array.reduce((prev, item, index) => {
+      return prev + item
+    },)
+    return arr / array.length
+
+  }
+};
